22a42c3d9977ee9fff2ea08fcd6c417f012f84b2
Sebastian Carbajal <143103571+LaLechuga18@users.noreply.github.com>
2024-04-25 07:28:34 -0600

    Add files via upload

---
+++ GUI.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+
+
+def abrir_archivo():
+    archivo = filedialog.LoadFileDialog(initialdir="/", title="Seleccionar", filetypes=(("Archivos texto", "*.txt"), ("Todos los archivos" , "*.*")))
+    print("Archivo", archivo)
+
+
+
+root = Tk()
+root.title("Huffman")
+root.geometry("200x300")
+
+
+ttk.Label(root, text="Codificacion de Huffman", font="Arial").grid(column=0,row=0)
+ttk.Button(root, text="Seleccionar Archivo", command=abrir_archivo).grid()
+ttk.Button(root, text="Comprimir", command=contar).grid()
+ttk.Button(root, text="Descomprimir").grid()
+ttk.Button(root, text="Salir", command=root.destroy).grid()
+
+
+root.mainloop()
