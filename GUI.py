416887e2849e73f86c11dc1b181120ff29fbc094
Sebastian Carbajal <143103571+LaLechuga18@users.noreply.github.com>
2024-04-29 18:56:54 -0600

    GUI.py

--- GUI.py
+++ GUI.py
@@ -16,7 +16,7 @@ root.geometry("200x300")
 
 ttk.Label(root, text="Codificacion de Huffman", font="Arial").grid(column=0,row=0)
 ttk.Button(root, text="Seleccionar Archivo", command=abrir_archivo).grid()
-ttk.Button(root, text="Comprimir", command=contar).grid()
+ttk.Button(root, text="Comprimir").grid()
 ttk.Button(root, text="Descomprimir").grid()
 ttk.Button(root, text="Salir", command=root.destroy).grid()
 
